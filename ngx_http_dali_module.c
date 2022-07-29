90ebeb63a1844b86fd4e515e96366cc0f9b25017
Will Hawkins <whh8b@obs.cr>
2022-07-29 19:15:00 -0400

    Remove support for sending JSON timing information.

--- ngx_http_dali_module.c
+++ ngx_http_dali_module.c
@@ -145,13 +145,9 @@ static ngx_int_t ngx_http_dali_handler(ngx_http_request_t *r) {
   ngx_http_dali_conf_t *conf = NULL;
   u_char *content_type = (u_char *)"application/octet-stream";
   size_t content_type_len = ngx_strlen(content_type);
-  char response_json_template[] =
-      "{\"DurationMS\": %.8f, \"Bytes\": %d, \"BPS\": %d}";
   ngx_uint_t status = NGX_HTTP_OK;
-  ngx_uint_t ngx_rc = NGX_OK;
-  struct timespec before_ts, after_ts;
-  u_char *response_json = NULL;
-  size_t response_json_len = 0;
+  ngx_int_t ngx_discard_rc = NGX_OK;
+  ngx_int_t ngx_send_header_rc = NGX_OK;
   ngx_chain_t *output_chain_head = NULL;
 
   r->headers_out.content_type.len = content_type_len;
@@ -175,55 +171,18 @@ static ngx_int_t ngx_http_dali_handler(ngx_http_request_t *r) {
   }
 
   if (conf) {
-    bool clock_success = true;
-    if (clock_gettime(CLOCK_MONOTONIC, &before_ts) < 0) {
-      ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0,
-                    "Dali could not measure the time it took to reading the "
-                    "client request's body (after)");
-      clock_success = false;
-    }
-
-    /*
-     * We could fail to read the body of the client's request.
-     */
-    ngx_rc = ngx_http_discard_request_body(r);
-    if (ngx_rc > NGX_OK) {
+    ngx_discard_rc = ngx_http_discard_request_body(r);
+    if (ngx_discard_rc > NGX_OK) {
       ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0,
                     "Dali could not read and discard the request's body");
     }
-
-    if (clock_gettime(CLOCK_MONOTONIC, &after_ts) < 0) {
-      ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0,
-                    "Dali could not measure the time it took to reading the "
-                    "client request's body (after)");
-      clock_success = false;
-    }
-
-    if (clock_success) {
-      struct timespec body_read_duration_ts = {};
-      double body_read_duration = 0;
-      off_t body_read_size;
-
-      body_read_duration_ts = diff_timespec(&before_ts, &after_ts);
-      body_read_duration = flatten_timespec_to_ms(&body_read_duration_ts);
-      body_read_size = r->request_body != NULL ? r->request_body->received : 0;
-
-      // First, calculate how big to make the buffer.
-      response_json_len = snprintf(NULL, 0, response_json_template,
-                                   body_read_duration, body_read_size, 0) +
-                          1;
-
-      // Second, use that calculation to make just enough space for the JSON
-      response_json = ngx_pcalloc(r->pool, sizeof(u_char) * response_json_len);
-
-      ngx_snprintf(response_json, response_json_len, response_json_template,
-                   body_read_duration, body_read_size, 0);
-    }
-
+  }
+ 
+  if (conf && ngx_discard_rc == NGX_OK) {
     ngx_chain_t *output_chain_iterator = NULL;
     for (size_t index = 0; index<conf->length; index++) {
       
-      ngx_buf_t *response_buffer = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+      ngx_buf_t *response_buffer = ngx_calloc_buf(r->pool);
       if (!response_buffer) {
         ngx_log_error(
             NGX_LOG_CRIT, r->connection->log, 0,
@@ -234,48 +193,39 @@ static ngx_int_t ngx_http_dali_handler(ngx_http_request_t *r) {
         break;
       }
       response_buffer->memory = 1;
+      ngx_chain_t *new_ngx_chain = ngx_pcalloc(r->pool, sizeof(ngx_chain_t));
 
       // Special case handling for the first response frame.
       if (index == 0) {
         ngx_log_error(
-            NGX_LOG_CRIT, r->connection->log, 0,
+            NGX_LOG_DEBUG, r->connection->log, 0,
             "Dali building first output chain entry.");
-        output_chain_head = ngx_pcalloc(r->pool, sizeof(ngx_chain_t));
+        output_chain_head = new_ngx_chain;
         output_chain_iterator = output_chain_head;
-
-        size_t first_output_len = RESPONSE_MULTIPLE + response_json_len;
-        u_char *first_output_buffer = ngx_pcalloc(r->pool, sizeof(u_char) * first_output_len);
-        ngx_log_error(
-            NGX_LOG_CRIT, r->connection->log, 0,
-            "first_output_len: %d.", first_output_len);
-        x_initialize(first_output_buffer, first_output_len);
-        ngx_memcpy(first_output_buffer, response_json, response_json_len);
-        response_buffer->pos = first_output_buffer;
-        response_buffer->last = first_output_buffer + first_output_len;
       } else {
         ngx_log_error(
-            NGX_LOG_CRIT, r->connection->log, 0,
+            NGX_LOG_DEBUG, r->connection->log, 0,
             "Dali building %d st/nd output chain entry.", (index + 1));
         ngx_chain_t *previous_chain = output_chain_iterator;
-        output_chain_iterator = ngx_pcalloc(r->pool, sizeof(ngx_chain_t));
+        output_chain_iterator = new_ngx_chain;
         previous_chain->next = output_chain_iterator;
-        response_buffer->pos = response_contents;
-        response_buffer->last = response_contents + RESPONSE_MULTIPLE;
       }
+      response_buffer->pos = response_contents;
+      response_buffer->last = response_contents + RESPONSE_MULTIPLE;
       response_buffer->last_buf = (index+1) == conf->length ? 1 : 0;
       output_chain_iterator->buf = response_buffer;
     }
-
   }
+
   /*
    * If any of those failures happened, bail out.
    */
-  if (!conf || ngx_rc > NGX_OK || !output_chain_head) {
+  if (!conf || ngx_discard_rc > NGX_OK || !output_chain_head) {
     status = NGX_HTTP_INTERNAL_SERVER_ERROR;
     r->headers_out.status = status;
     r->headers_out.content_length_n = 0;
     ngx_http_send_header(r);
-    return NGX_ERROR;
+    return status;
   }
 
   ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "Dali module responding");
@@ -291,17 +241,21 @@ static ngx_int_t ngx_http_dali_handler(ngx_http_request_t *r) {
    * Setup some values for the header of our response.
    */
   r->headers_out.content_length_n =
-      sizeof(u_char) * ((conf->length*RESPONSE_MULTIPLE) + response_json_len);
+      sizeof(u_char) * (conf->length*RESPONSE_MULTIPLE);
   r->headers_out.status = status;
 
   /*
    * Send the headers of the response.
    */
-  ngx_rc = ngx_http_send_header(r);
-  if (ngx_rc > NGX_OK) {
+  ngx_send_header_rc = ngx_http_send_header(r);
+  if (ngx_send_header_rc == NGX_ERROR) {
     ngx_log_error(NGX_LOG_CRIT, r->connection->log, 0,
                   "Dali could not send the response header");
-    return ngx_rc;
+    return ngx_send_header_rc;
+  }
+
+  if (ngx_send_header_rc > NGX_OK || r->header_only) {
+    return ngx_send_header_rc;
   }
 
   /*
@@ -328,12 +282,15 @@ static ngx_int_t ngx_http_dali_handler(ngx_http_request_t *r) {
 static void *ngx_http_dali_create_conf(ngx_conf_t *cf) {
   ngx_http_dali_conf_t *conf;
 
+  ngx_log_error(NGX_LOG_DEBUG, cf->log, 0,
+                "ngx_http_dali_create_conf starting");
   conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_dali_conf_t));
   if (conf == NULL) {
     return NULL;
   }
   conf->length = NGX_CONF_UNSET_SIZE;
-
+  ngx_log_error(NGX_LOG_DEBUG, cf->log, 0,
+                "ngx_http_dali_create_conf returning: %uxL", (uintptr_t)conf);
   return conf;
 }
 
@@ -387,7 +344,13 @@ static char *ngx_http_dali_merge_conf(ngx_conf_t *cf, void *parent,
  */
 static char *ngx_http_dali_enable(ngx_conf_t *cf, ngx_command_t *cmd,
                                   void *conf) {
+  ngx_log_error(NGX_LOG_DEBUG, cf->log, 0,
+                "ngx_http_dali_enable starting (conf: %uxL)", (uintptr_t)conf);
   ngx_http_core_loc_conf_t *clcf;
+
+  // Behind the scenes there is a tremendous amount of trickery required
+  // in order to make this module implementation work out properly. In other
+  // words, don't think too deeply about it.
   clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
 
   /*
